f7ce7ca79c554938b649b1e77aef938b02a99bc2
Yixin Jiang <91864970+yij011@users.noreply.github.com>
2021-10-23 21:40:50 -0700

    add explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,71 @@
 // explore.js
+var synth = window.speechSynthesis;
+var Button= document.querySelector('button');
+var inputTxt = document.querySelector('#text-to-speak');
+var voiceSelect = document.querySelector('select');
+var voices = [];
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  voiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = voiceList;
+  }
+}
+
+function voiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      if ( aname < bname ) return -1;
+      else if ( aname == bname ) return 0;
+      else return +1;
+  });
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+function speak(){
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (inputTxt.value !== '') {
+      var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+      utterThis.onend = function(event) {
+      console.log('SpeechSynthesisUtterance.onend');
+    }
+    utterThis.onerror = function(event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utterThis);
+  }
+}
+
+Button.onclick = function(event) {
+  event.preventDefault();
+
+  speak();
+
+  inputTxt.blur();
 }
